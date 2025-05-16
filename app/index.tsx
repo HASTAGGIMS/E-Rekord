142544422132304f93ed189fc6d735e0c3404240
HASTAGGIMS <jacobarmstrong61890@gmail.com>
2025-05-16 22:46:08 +0800

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../auth.styles";
+
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+
+      <Text style={styles.title}>HELLO</Text>
+    
+    <TouchableOpacity onPress={() =>alert("Gegege")}>
+      <Text>Hikapa ge</Text>
+    </TouchableOpacity>
+
+    <Image 
+      source ={require("../assets/images/react-logo.png")}
+      style={{
+        height: 200,
+        width:200
+      }}
+    />
+    </View>
+  );
+}
